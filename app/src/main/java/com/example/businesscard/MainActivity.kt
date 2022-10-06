8eae32542d41f43306816b69af947b4b1ba1aa6c
Vinícius Silveira <vinifs19@gmail.com>
2022-10-06 19:44:39 -0300

    Added Android logo, my name and title

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.businesscard
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +31,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    BusinessCard()
 
                 }
             }
         }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+    val image = painterResource(R.drawable.android_logo)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .wrapContentHeight(Alignment.CenterVertically)
+                .size(48.dp),
+            contentScale = ContentScale.Crop
+
+        )
+        Text(
+            text = "Vinicius Silveira",
+            fontSize = 40.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "Android Developer",
+            fontSize = 32.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+        )
     }
 }
 
@@ -35,6 +75,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
+        BusinessCard()
 
     }
 }
